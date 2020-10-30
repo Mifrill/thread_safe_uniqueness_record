aa3a3b6888640b1c8d3d0683942fbf45d90a693a
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2020-10-30 11:43:59 +0500

    initialize gem skeleton

---
+++ lib/thread_safe_uniqueness_record.rb
@@ -0,0 +1,6 @@
+require "thread_safe_uniqueness_record/version"
+
+module ThreadSafeUniquenessRecord
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/thread_safe_uniqueness_record/version.rb
@@ -0,0 +1,3 @@
+module ThreadSafeUniquenessRecord
+  VERSION = "0.1.0"
+end
